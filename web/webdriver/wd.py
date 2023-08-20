37dae5f2f466ead1fcbe5b94168b845a24924a65
Derek Xu <derekxu2016k@gmail.com>
2023-08-20 02:15:40 -0700

    Added Scraping Application URLs

--- web/webdriver/wd.py
+++ web/webdriver/wd.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import pandas
@@ -18,79 +17,87 @@ def remove_duplicate_dicts(input_list):
 
 def scrape(target_url):
     o = {}
+    driver = webdriver.Chrome()
 
-    with webdriver.Chrome() as driver:
-        driver.get(target_url)
+    driver.get(target_url)
 
-        driver.maximize_window()
-        time.sleep(2)
+    driver.maximize_window()
+    time.sleep(2)
 
-        allJobsContainer = driver.find_element(By.CLASS_NAME, 'exy0tjh5')
-        
-        allJobs = allJobsContainer.find_elements(By.TAG_NAME, 'li')
+    resp = driver.page_source
 
-        for job in allJobs:
-            job_source = job.get_attribute("outerHTML")
+    driver.close()
 
-            job_soup = BeautifulSoup(job_source, "html.parser")
+    soup = BeautifulSoup(resp, "html.parser")
 
-            try:
-                name = job_soup.find("div", {"class":"job-search-gx72iw"}).text
-                name = re.sub(r'[^a-zA-Z\s]', '', name)
-                o["name-of-company"]=name
-            except:
-                o["name-of-company"]=None
+    allJobsContainer = soup.find("ul", {"class":"css-7ry9k1"})
 
-            try:
-                name = job_soup.find("div",{"class":"job-title mt-xsm"}).text
-                name = re.sub(r',', '', name)
+    allJobs = allJobsContainer.find_all("li")
 
-                lower = name.lower()
 
-                if (("software" in lower or "develop" in lower) and "intern" in name.lower()):
-                    o["name-of-job"]=name
-                else:
-                    continue
-            except:
-                o["name-of-job"]=None
+    for job in allJobs:
+        try:
+            name = job.find("div", {"class":"job-search-gx72iw"}).text
+            name = re.sub(r'[^a-zA-Z\s]', '', name)
+            o["name-of-company"]=name
+        except:
+            o["name-of-company"]=None
 
-            try:
-                location = job_soup.find("div",{"class":"location mt-xxsm"}).text
-                location = re.sub(r',', '', location)
-                o["location"]=location
-            except:
-                o["location"]=None
+        try:
+            name = job.find("div",{"class":"job-title mt-xsm"}).text
+            name = re.sub(r',', '', name)
 
-            try:
-                salary = job_soup.find("div",{"class":"salary-estimate"}).text
-                salary = re.sub(r',','', salary)
-                o["salary"]=salary
-            except:
-                o["salary"]=None
+            lower = name.lower()
 
-            try:
-                o["date"] = job_soup.find("div",{"class":"listing-age"}).text
-            except:
-                o["date"]=None
+            if (("software" in lower or "develop" in lower) and "intern" in name.lower()):
+                o["name-of-job"]=name
+            else:
+                continue
+        except:
+            o["name-of-job"]=None
 
-            job.click()
+        try:
+            location = job.find("div",{"class":"location mt-xxsm"}).text
+            location = re.sub(r',', '', location)
+            o["location"]=location
+        except:
+            o["location"]=None
 
-            extended_description = driver.find_element(By.CLASS_NAME, 'css-1d88wr9')
+        try:
+            salary = job.find("div",{"class":"salary-estimate"}).text
+            salary = re.sub(r',','', salary)
+            o["salary"]=salary
+        except:
+            o["salary"]=None
 
-            driver.implicitly_wait(2)
+        try:
+            o["date"] = job.find("div",{"class":"listing-age"}).text
+        except:
+            o["date"]=None
 
-            description_source = extended_description.get_attribute("outerHTML")
+        extendedDescriptionLinkElement = job.find("a")
 
-            description_soup = BeautifulSoup(description_source, "html.parser")
+        extendedDescriptionLink = "https://www.glassdoor.com" + extendedDescriptionLinkElement["href"]
 
-            try:
-                app_link_element = description_soup.find("a", {"class": "css-akpsfi"})
-                o["application-link"] = app_link_element.get("href")
-            except:
-                o["application-link"] = None
+        extendedDescription = webdriver.Chrome()
 
-            l.append(o)
-            o = {}
+        extendedDescription.get(extendedDescriptionLink)
+
+        extendedDescriptionSource = extendedDescription.page_source
+        '''x=extendedDescriptionSource.index('data-easy-apply')
+        print(extendedDescriptionSource[x-150:x+250])'''
+
+        extendedDescriptionSoup = BeautifulSoup(extendedDescriptionSource, "html.parser")
+
+        try:
+            appLinkElement = extendedDescriptionSoup.find("button", {"data-easy-apply": "false"})
+            o["application-link"] = "https://www.glassdoor.com" + appLinkElement["data-job-url"]
+            print(o["application-link"])
+        except:
+            o["application-link"] = None
+
+        l.append(o)
+        o = {}
 
 target_url = ["https://www.glassdoor.com/Job/irvine-python-intern-jobs-SRCH_IL.0,6_IC1146798_KO7,20.htm?radius=100","https://www.glassdoor.com/Job/irvine-software-intern-jobs-SRCH_IL.0,6_IC1146798_KO7,22.htm?radius=100"]
 
