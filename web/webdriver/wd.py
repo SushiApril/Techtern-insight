e25ba71d80fbee3a3d5dbdf44a6985d28cc55fa3
derekxu2023k <dkxu@uci.edu>
2023-08-24 22:53:06 -0700

    BeautifulSoup scrape experiment 2

--- web/webdriver/wd.py
+++ web/webdriver/wd.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+from random import random
 import requests
 import time
 import pandas
@@ -37,6 +38,8 @@ def scrape(target_url):
 
 
     for job in allJobs:
+        time.sleep(random())
+
         try:
             name = job.find("div", {"class":"job-search-gx72iw"}).text
             name = re.sub(r'[^a-zA-Z\s]', '', name)
@@ -81,11 +84,24 @@ def scrape(target_url):
         extendedDescriptionLink = "https://www.glassdoor.com" + extendedDescriptionLinkElement["href"]
 
         extendedDescriptionResponse = requests.get(extendedDescriptionLink, headers={
-            'User-Agent': 'TechTern Scraper',
-            'Referer': 'https://www.example.com',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept-Encoding': 'gzip, deflate',
-            'Connection': 'keep-alive'})
+            "User-Agent": "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+            "Accept-Encoding": "gzip, deflate, br", 
+            "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7", 
+            "Dnt": "1", 
+            "Host": "glassdoor.com", 
+            "Referer": target_url, 
+            "Sec-Ch-Ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"", 
+            "Sec-Ch-Ua-Mobile": "?0", 
+            "Sec-Ch-Ua-Platform": "\"Windows\"", 
+            "Sec-Fetch-Dest": "document", 
+            "Sec-Fetch-Mode": "navigate", 
+            "Sec-Fetch-Site": "cross-site", 
+            "Sec-Fetch-User": "?1", 
+            "Upgrade-Insecure-Requests": "1", 
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36", 
+            "X-Amzn-Trace-Id": "Root=1-64e83661-0ad85904753e7cc54d34580c",
+        })
 
         extendedDescriptionSoup = BeautifulSoup(extendedDescriptionResponse.text, "html.parser")
 
