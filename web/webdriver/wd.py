106ae9f6b3d17bc68184939455c0a423fa792cb7
derekxu2023k <dkxu@uci.edu>
2023-08-25 03:08:04 -0700

    Added traversing result pages

--- web/webdriver/wd.py
+++ web/webdriver/wd.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import time
 import pandas
 import re
+from pprint import pprint
 
 PATH = ".\chromedriver.exe"
 l = list()
@@ -16,84 +17,97 @@ def remove_duplicate_dicts(input_list):
     return unique_dicts
 
 def scrape(target_url):
-    o = {}
-    driver = webdriver.Chrome()
+    with webdriver.Chrome() as driver:
+        driver.get(target_url)
 
-    driver.get(target_url)
+        driver.maximize_window()
 
-    driver.maximize_window()
-    time.sleep(2)
+        pages_left = True
 
-    resp = driver.page_source
+        while pages_left:
+            time.sleep(2)
 
-    soup = BeautifulSoup(resp, "html.parser")
+            resp = driver.find_element(By.CLASS_NAME, "css-152bcv1")
 
-    allJobsContainer = soup.find("ul", {"class":"css-7ry9k1"})
+            respSource = resp.get_attribute("innerHTML")
 
-    allJobs = allJobsContainer.find_all("li")
+            soup = BeautifulSoup(respSource, "html.parser")
 
-    jobLinkElements = driver.find_elements(By.CLASS_NAME, "eigr9kq3")
+            allJobsContainer = soup.find("ul", {"class":"css-7ry9k1"})
 
-    for job, jobLink in zip(allJobs, jobLinkElements):
-        try:
-            name = job.find("div", {"class":"job-search-gx72iw"}).text
-            name = re.sub(r'[^a-zA-Z\s]', '', name)
-            o["name-of-company"]=name
-        except:
-            o["name-of-company"]=None
+            allJobs = allJobsContainer.find_all("li")
 
-        try:
-            name = job.find("div",{"class":"job-title mt-xsm"}).text
-            name = re.sub(r',', '', name)
+            jobLinkElements = driver.find_elements(By.CLASS_NAME, "eigr9kq3")
 
-            lower = name.lower()
+            for job, jobLink in zip(allJobs, jobLinkElements):
+                o = {}
 
-            if (("software" in lower or "develop" in lower) and "intern" in name.lower()):
-                o["name-of-job"]=name
+                try:
+                    name = job.find("div",{"class":"job-title mt-xsm"}).text
+                    name = re.sub(r',', '', name)
+
+                    lower = name.lower()
+
+                    if (("software" in lower or "develop" in lower) and "intern" in name.lower()):
+                        o["name-of-job"]=name
+                    else:
+                        continue
+                except:
+                    o["name-of-job"]=None
+
+                try:
+                    location = job.find("div",{"class":"location mt-xxsm"}).text
+                    location = re.sub(r',', '', location)
+                    o["location"]=location
+                except:
+                    o["location"]=None
+
+                try:
+                    salary = job.find("div",{"class":"salary-estimate"}).text
+                    salary = re.sub(r',','', salary)
+                    o["salary"]=salary
+                except:
+                    o["salary"]=None
+
+                try:
+                    o["date"] = job.find("div",{"class":"listing-age"}).text
+                except:
+                    o["date"]=None
+
+                driver.implicitly_wait(1)
+
+                try:
+                    cancelModalButton = driver.find_element(By.CLASS_NAME, "e1jbctw80")
+                    cancelModalButton.click()
+                except:
+                    pass
+
+                jobLink.click()
+
+                try:
+                    name = driver.find_element(By.CSS_SELECTOR, '[data-test="employerName"]')
+                    print(1)
+                    name = name.text.split('\n')[0]
+                    print(2)
+                    o["name-of-company"]=name
+                    print(o["name-of-company"])
+                except:
+                    o["name-of-company"]=None
+
+                try:
+                    app_link = driver.find_element(By.CSS_SELECTOR, '[data-easy-apply="false"]')
+                    o["application-link"] = "https://www.glassdoor.com" + app_link.get_attribute("data-job-url")
+                except:
+                    o["application-link"] = None
+
+                l.append(o)
+
+            nextButton = driver.find_element(By.CLASS_NAME, "nextButton")
+            
+            if nextButton.get_attribute("disabled") == None:
+                nextButton.click()
             else:
-                continue
-        except:
-            o["name-of-job"]=None
-
-        try:
-            location = job.find("div",{"class":"location mt-xxsm"}).text
-            location = re.sub(r',', '', location)
-            o["location"]=location
-        except:
-            o["location"]=None
-
-        try:
-            salary = job.find("div",{"class":"salary-estimate"}).text
-            salary = re.sub(r',','', salary)
-            o["salary"]=salary
-        except:
-            o["salary"]=None
-
-        try:
-            o["date"] = job.find("div",{"class":"listing-age"}).text
-        except:
-            o["date"]=None
-
-        try:
-            cancelModalButton = driver.find_element(By.CLASS_NAME, "e1jbctw80")
-            cancelModalButton.click()
-        except:
-            pass
-
-        jobLink.click()
-
-        driver.implicitly_wait(1)
-
-        try:
-            app_link = driver.find_element(By.CSS_SELECTOR, "[data-apply-type]")
-            o["application-link"] = "https://www.glassdoor.com" + app_link.get_attribute("data-job-url")
-        except:
-            o["application-link"] = None
-
-        l.append(o)
-        o = {}
-
-    driver.close()
+                pages_left = False
 
 target_url = ["https://www.glassdoor.com/Job/irvine-python-intern-jobs-SRCH_IL.0,6_IC1146798_KO7,20.htm?radius=100","https://www.glassdoor.com/Job/irvine-software-intern-jobs-SRCH_IL.0,6_IC1146798_KO7,22.htm?radius=100"]
 
@@ -103,7 +117,9 @@ for url in target_url:
 
 l = remove_duplicate_dicts(l)
 
-sorted_list = sorted(l, key=lambda x: x["name-of-company"])
+pprint(l)
+
+sorted_list = sorted(l, key=lambda x: '' if x["name-of-company"] == None else x["name-of-company"])
 
 df = pandas.DataFrame(sorted_list)
 df.to_csv('jobtest.csv', index = False, encoding = "utf-8")
