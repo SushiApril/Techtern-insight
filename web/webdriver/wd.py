5e26d442e87472d7da1e1341b87e9c37208c8fd1
derekxu2023k <dkxu@uci.edu>
2023-09-03 18:06:20 -0700

    Scraper Optimization Phase 1

--- web/webdriver/wd.py
+++ web/webdriver/wd.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 import pandas
 import re
-from pprint import pprint
 
 PATH = ".\chromedriver.exe"
-l = list()
+l = []
 
 def remove_duplicate_dicts(input_list):
     unique_dicts = []
@@ -16,15 +17,27 @@ def remove_duplicate_dicts(input_list):
             unique_dicts.append(d)
     return unique_dicts
 
-def scrape(target_url):
+def scrape(target_url, max_pgs=5):
     with webdriver.Chrome() as driver:
+        curr_pg = 1
+
         driver.get(target_url)
 
         driver.maximize_window()
 
-        pages_left = True
+        pagesLeft = True
 
-        while pages_left:
+        removeModalCSS = '#LoginModal {display: none;}'
+
+        removeModalJS = f'''
+                         var scriptRemoveStyle = document.createElement("style");
+                         scriptRemoveStyle.innerHTML = "{removeModalCSS}";
+                         document.head.appendChild(scriptRemoveStyle);
+                         '''
+
+        driver.execute_script(removeModalJS)
+
+        while pagesLeft:
             time.sleep(2)
 
             resp = driver.find_element(By.CLASS_NAME, "css-152bcv1")
@@ -41,6 +54,17 @@ def scrape(target_url):
 
             for job, jobLink in zip(allJobs, jobLinkElements):
                 o = {}
+
+                jobLink.click()
+
+                try:
+                    waitForElem = WebDriverWait(driver, 5)
+                    name = waitForElem.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-test="employerName"]')))
+                    name = name.text.split("\n")[0]
+                    name = re.sub(r',', '', name)
+                    o["name-of-company"]=name
+                except:
+                    o["name-of-company"]=None
 
                 try:
                     name = job.find("div",{"class":"job-title mt-xsm"}).text
@@ -74,26 +98,6 @@ def scrape(target_url):
                 except:
                     o["date"]=None
 
-                driver.implicitly_wait(1)
-
-                try:
-                    cancelModalButton = driver.find_element(By.CLASS_NAME, "e1jbctw80")
-                    cancelModalButton.click()
-                except:
-                    pass
-
-                jobLink.click()
-
-                try:
-                    name = driver.find_element(By.CSS_SELECTOR, '[data-test="employerName"]')
-                    print(1)
-                    name = name.text.split('\n')[0]
-                    print(2)
-                    o["name-of-company"]=name
-                    print(o["name-of-company"])
-                except:
-                    o["name-of-company"]=None
-
                 try:
                     app_link = driver.find_element(By.CSS_SELECTOR, '[data-easy-apply="false"]')
                     o["application-link"] = "https://www.glassdoor.com" + app_link.get_attribute("data-job-url")
@@ -103,12 +107,14 @@ def scrape(target_url):
                 l.append(o)
 
             nextButton = driver.find_element(By.CLASS_NAME, "nextButton")
-            
-            if nextButton.get_attribute("disabled") == None:
-                nextButton.click()
-            else:
-                pages_left = False
 
+            print(curr_pg)
+            if nextButton.get_attribute("disabled") == None and curr_pg != max_pgs:
+                nextButton.click()
+                curr_pg += 1
+            else:
+                pagesLeft = False
+ 
 target_url = ["https://www.glassdoor.com/Job/irvine-python-intern-jobs-SRCH_IL.0,6_IC1146798_KO7,20.htm?radius=100","https://www.glassdoor.com/Job/irvine-software-intern-jobs-SRCH_IL.0,6_IC1146798_KO7,22.htm?radius=100"]
 
 for url in target_url:
@@ -117,9 +123,7 @@ for url in target_url:
 
 l = remove_duplicate_dicts(l)
 
-pprint(l)
-
 sorted_list = sorted(l, key=lambda x: '' if x["name-of-company"] == None else x["name-of-company"])
 
 df = pandas.DataFrame(sorted_list)
-df.to_csv('jobtest.csv', index = False, encoding = "utf-8")
+df.to_csv('web/jobtest.csv', index = False, encoding = "utf-8")
