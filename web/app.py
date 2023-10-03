107f16aefde1418d874ba2f70674c437f12110ca
alrumaihif <fai.alrumaihi7@gmail.com>
2023-10-03 08:42:55 -0700

    updating app.py accordingly

--- web/app.py
+++ web/app.py
@@ -29,7 +29,6 @@ def stats():
     df = pd.read_csv("https://raw.githubusercontent.com/SushiApril/Techtern-insight/rebuild-site-with-flask/jobs.csv")
     df = clean_data(df)
 
-    days_since_posted = plot_days_since_posted(df)
     avg_salary_distribution = plot_avg_salary_distribution(df)
     jobs_by_state = plot_jobs_by_state(df)
     top_cities_by_jobs = plot_top_cities_by_jobs(df)
@@ -39,7 +38,7 @@ def stats():
     salary_boxplot_by_state = plot_salary_boxplot_by_state(df)
     company_wise_job_postings = plot_company_wise_job_postings(df)
 
-    return render_template("stats.html", days_since_posted=days_since_posted, avg_salary_distribution=avg_salary_distribution,
+    return render_template("stats.html",  avg_salary_distribution=avg_salary_distribution,
                            jobs_by_state=jobs_by_state, top_cities_by_jobs=top_cities_by_jobs, avg_salary_by_state=avg_salary_by_state,
                            salaries_by_state=salaries_by_state, salary_density_by_state=salary_density_by_state, salary_boxplot_by_state=salary_boxplot_by_state, 
                            company_wise_job_postings=company_wise_job_postings)
