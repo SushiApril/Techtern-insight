70353b1f0105c933c02baabbed7a93849bd3a0dc
derekxu2023k <dkxu@uci.edu>
2023-10-08 01:57:43 -0700

    Update scraper to new UI, update date column, make scraper automatic

--- web/app.py
+++ web/app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 import pandas as pd
 import sqlite3
 from eda import *
+from datetime import datetime, timedelta
+import threading
+from importlib.util import spec_from_file_location, module_from_spec
+
+spec = spec_from_file_location("wd", "web\webdriver\wd.py")
+
+wd = module_from_spec(spec)
+
+spec.loader.exec_module(wd)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,6 +20,8 @@ def index():
 
 @app.route('/datasite')
 def datasite():
+    TODAY = datetime.today().date()
+
     with sqlite3.connect('web/jobs.db') as conn:
         cursor = conn.cursor()
         cursor.execute('SELECT name_of_company, name_of_job, location, salary, date, application_link FROM jobs')
@@ -18,7 +30,7 @@ def datasite():
             while job := cursor.fetchone():
                 yield enumerate(job)
 
-        return render_template('datasite.html', jobs=jobsGenerator())
+        return render_template('datasite.html', timedelta=timedelta, jobs=jobsGenerator(), today=TODAY)
 
 @app.route('/about')
 def about():
@@ -41,6 +53,33 @@ def stats():
     return render_template("stats.html",  avg_salary_distribution=avg_salary_distribution,
                            jobs_by_state=jobs_by_state, top_cities_by_jobs=top_cities_by_jobs, avg_salary_by_state=avg_salary_by_state,
                            salaries_by_state=salaries_by_state, salary_density_by_state=salary_density_by_state, salary_boxplot_by_state=salary_boxplot_by_state, 
-                           company_wise_job_postings=company_wise_job_postings)      
-if __name__ == '__main__':
+                           company_wise_job_postings=company_wise_job_postings)  
+
+def run():
     app.run(port=5004)
+
+def get_data_periodically():
+    sched_time = 60 * (1 * 60 + 50)
+
+    while True:
+        now = datetime.now()
+
+        seconds = 3600 * now.hour + now.minute * 60 + now.second
+
+        timer_until_midnight = threading.Timer(sched_time - seconds, wd.get_data)
+
+        timer_until_midnight.start()
+
+        timer_until_midnight.join()
+
+        sched_time = 86400
+
+def main():
+    application = threading.Thread(target=run)
+    scrape = threading.Thread(target=get_data_periodically)
+
+    application.start()
+    scrape.start()
+
+if __name__ == '__main__':
+    main()
--- web/webdriver/wd.py
+++ web/webdriver/wd.py
@@ -1,22 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from collections import OrderedDict
+from pathlib import Path
 import re
 import sqlite3
+import traceback
 
 '''
 PENDING TASKS
 
--Add easy apply URL as the job listing's own page
--Use WebDriverWait for going between pages???
+-Use WebDriverWait for going between pages - still needs improvement?? But seems to work now
 '''
 
 def setup_database():
-    open('./web/jobs.db', 'w')
-    conn = sqlite3.connect('./web/jobs.db') # Creates a new SQLite file named 'jobs.db'
+    open('./web/jobs_temp.db', 'w')
+    conn = sqlite3.connect('./web/jobs_temp.db') # Creates a new SQLite file named 'jobs_temp.db'
     c = conn.cursor()
     
     # Create a new table
@@ -37,19 +39,15 @@ def setup_database():
     conn.commit()
     conn.close()
 
-def scrape(target_url, maxPgs=5):
+def scrape(target_url, loads=5):
     with webdriver.Chrome() as driver:
-        currPg = 1
-
         driver.get(target_url)
 
         driver.maximize_window()
 
-        pagesLeft = True
-
-        removeModalCSS = '#LoginModal {display: none;}'
+        removeModalCSS = '.gd-ui-modal, .ModalContainer, #LoginModal {display: none;}'
         
-        #this is for scraping glassdoor without logging in. Prvenet login popup
+        #this is for scraping Glassdoor without logging in. Prvenet login popup
         removeModalJS = f'''
                          var scriptRemoveStyle = document.createElement("style");
                          scriptRemoveStyle.innerHTML = "{removeModalCSS}";
@@ -60,119 +58,123 @@ def scrape(target_url, maxPgs=5):
 
         wait1Min = WebDriverWait(driver, 60)
 
-        while pagesLeft:
-            #checks if its the correct page to scrape
-            def correctPage(driver):
+        def button_found(buttonContainer):
+            try:
+                button = buttonContainer.find_element(By.CLASS_NAME, 'button_Button__meEg5')
+
+                return button.get_attribute("data-loading") == "false"
+            except NoSuchElementException:
+                return False
+
+        for _ in range(loads - 1):
+            buttonContainer = wait1Min.until(EC.presence_of_element_located((By.CLASS_NAME, "JobsList_buttonWrapper__haBp5")))
+
+            waitForButton = WebDriverWait(buttonContainer, 60)
+
+            waitForButton.until(button_found)
+
+            loadMoreButton = buttonContainer.find_element(By.CLASS_NAME, 'button_Button__meEg5')
+
+            loadMoreButton.click()
+
+        resp = driver.find_element(By.CLASS_NAME, "JobsList_jobsList__Ey2Vo")
+
+        respSource = resp.get_attribute("innerHTML")
+
+        soup = BeautifulSoup(respSource, "html.parser")
+
+        # BS
+        allJobs = soup.find_all("li")
+        # Selenium
+        jobLinkElements = driver.find_elements(By.CLASS_NAME, "jobCard")
+
+        button_has_link = lambda button: button.get_attribute("data-job-url")
+
+        for job, jobLink in zip(allJobs, jobLinkElements):
+            o = OrderedDict()
+
+            try:
                 try:
-                    pageNumElement = driver.find_element(By.CLASS_NAME, 'paginationFooter')
-                    return f'Page {currPg}' in pageNumElement.text
-                except NoSuchElementException:
-                    return False
-
-            wait1Min.until(correctPage)
-
-            resp = driver.find_element(By.CLASS_NAME, "css-152bcv1")
-
-            respSource = resp.get_attribute("innerHTML")
-
-            soup = BeautifulSoup(respSource, "html.parser")
-
-            # BS
-            allJobsContainer = soup.find("ul", {"class":"css-7ry9k1"})
-            # BS
-            allJobs = allJobsContainer.find_all("li")
-            # Selenium
-            jobLinkElements = driver.find_elements(By.CLASS_NAME, "eigr9kq3")
-
-            for job, jobLink in zip(allJobs, jobLinkElements):
-                o = OrderedDict()
-
-                try:
-                    nameInfo = {}
-
-                    for info, tag, classId in zip(('name', 'rating'), ('div', 'span'), ('8wag7x', 'rnnx2x')):
-                        for idPrefix in ('css', 'job-search'):
-                            try:
-                                nameInfo[info] = job.find(tag,{"class":f"{idPrefix}-{classId}"}).text
-                            except:
-                                continue
-                            else:
-                                break
-
-                    name = nameInfo['name'].replace(nameInfo['rating'], '')
-                    o["name-of-company"]=name.title()
+                    name = job.find("div",{"class":"css-8wag7x"}).text
                 except:
-                    o["name-of-company"]=None
+                    name = job.find("div",{"class":"css-1bgdn7m"}).text
 
                 try:
-                    name = job.find("div",{"class":"job-title mt-xsm"}).text
-                    name = re.sub(r',', '', name)
-
-                    lower = name.lower()
-
-                    if (("software" in lower or "develop" in lower) and "intern" in name.lower()):
-                        o["name-of-job"]=name
-                    else:
-                        continue
+                    rating = job.find("span",{"class":"css-rnnx2x"}).text
+                    name = name.replace(rating, "")
                 except:
-                    o["name-of-job"]=None
+                    pass
 
+                o["name-of-company"]=name.title()
+            except:
+                o["name-of-company"]=None
+
+            try:
+                name = job.find("a",{"class":"css-1nh9iuj"}).text
+                name = re.sub(r',', '', name)
+
+                lower = name.lower()
+
+                if (("software" in lower or "develop" in lower) and "intern" in name.lower()):
+                    o["name-of-job"]=name
+                else:
+                    continue
+            except:
+                o["name-of-job"]=None
+
+            try:
+                location = job.find("div",{"class":"location mt-xxsm"}).text
+                location = re.sub(r',', '', location)
+                o["location"]=location
+            except:
+                o["location"]=None
+
+            try:
+                salary = job.find("div",{"class":"salary-estimate"}).text
+                salary = re.sub(r',','', salary)
+                o["salary"]=salary
+            except:
+                o["salary"]=None
+
+            try:
+                date = job.find("div",{"class":"listing-age"}).text
+
+                if date[-1] == 'd':
+                    numDays = int(date[:-1]) + 1
+                elif date == '24h':
+                    numDays = 1
+                elif date == '30d+':
+                    numDays = 32
+                else:
+                    numDays = None
+
+                o["date"]=numDays
+            except:
+                o["date"]=None
+
+            jobLink.click()
+
+            try:
                 try:
-                    location = job.find("div",{"class":"location mt-xxsm"}).text
-                    location = re.sub(r',', '', location)
-                    o["location"]=location
+                    appLinkElement = driver.find_element(By.CSS_SELECTOR, '[data-easy-apply="false"]')
+
+                    waitForLink = WebDriverWait(appLinkElement, 60)
+
+                    waitForLink.until(button_has_link)
+
+                    linkPath = appLinkElement.get_attribute("data-job-url")
                 except:
-                    o["location"]=None
+                    appLinkElement = job.find("a",{"class":"css-1nh9iuj"})
+                    linkPath = appLinkElement.get('href')
 
-                try:
-                    salary = job.find("div",{"class":"salary-estimate"}).text
-                    salary = re.sub(r',','', salary)
-                    o["salary"]=salary
-                except:
-                    o["salary"]=None
+                o["application-link"] = "https://www.glassdoor.com" + linkPath
+            except:
+                o["application-link"] = None
 
-                try:
-                    date = job.find("div",{"class":"listing-age"}).text
-
-                    if date[-1] == 'd':
-                        numDays = int(date[:-1])  
-                    elif date == '24h':
-                        numDays = 0
-                    elif date == '30d+':
-                        numDays = 31
-                    else:
-                        numDays = None
-
-                    o["date"] = numDays
-                except:
-                    o["date"]=None
-
-                jobLink.click()
-
-                try:
-                    try:
-                        appLinkElement = driver.find_element(By.CSS_SELECTOR, '[data-easy-apply="false"]')
-                        linkPath = appLinkElement.get_attribute("data-job-url")
-                    except:
-                        appLinkElement = job.find("a",{"class":"jobCard"})
-                        linkPath = appLinkElement.get('href')
-
-                    o["application-link"] = "https://www.glassdoor.com" + linkPath
-                except:
-                    o["application-link"] = None
-
-                insert_into_db(tuple(o.values()))
-
-            nextButton = driver.find_element(By.CLASS_NAME, "nextButton")
-
-            if nextButton.get_attribute("disabled") == None and currPg != maxPgs:
-                nextButton.click()
-                currPg += 1
-            else:
-                pagesLeft = False
+            insert_into_db(tuple(o.values()))
 
 def insert_into_db(position):
-    conn = sqlite3.connect('./web/jobs.db')
+    conn = sqlite3.connect('./web/jobs_temp.db')
     c = conn.cursor()
     try:
         c.execute('''
@@ -186,7 +188,7 @@ def insert_into_db(position):
         print("Duplicate entry detected - ", position, str(err))
     conn.close()
 
-def main():
+def get_data():
     PATH = ".\chromedriver.exe"
     setup_database()
     _home = "https://www.glassdoor.com/Job/software-intern-jobs-SRCH_KO0,15.htm"
@@ -195,10 +197,10 @@ def main():
     try:
         for url in targetUrl:
             scrape(url,30)
-    except:
-        print("An error happened")
+    except Exception as err:
+        print(traceback.format_exc())
 
-    with sqlite3.connect('./web/jobs.db') as conn:
+    with sqlite3.connect('./web/jobs_temp.db') as conn:
         cursor = conn.cursor()
         cursor.execute('SELECT name_of_company, name_of_job, location, salary, date, application_link FROM jobs ORDER BY date ASC;')
 
@@ -213,5 +215,11 @@ def main():
                 VALUES (?, ?, ?, ?, ?, ?)
                 ''', tuple(row))
             
-if __name__=="__main__":
-    main()
+    db_for_website = Path("./web/jobs.db")
+    temp_db = Path("./web/jobs_temp.db")
+
+    with temp_db.open("rb") as src:
+        content = src.read()
+
+    with db_for_website.open("wb") as dest:
+        dest.write(content)
